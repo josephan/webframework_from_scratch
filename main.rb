f18163b078bdea957e6ad72ee535f48a4471927f
Joseph An <joseph.an@thescore.com>
2018-04-14 00:09:03 -0400

    Database library

--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
 require_relative 'framework'
+require_relative 'database'
+require_relative 'queries'
+
+DB = Database.connect('postgres://localhost/framework_dev', QUERIES)
 
 APP = App.new do
   get '/' do
@@ -7,5 +11,15 @@ APP = App.new do
 
   get '/users/:username' do |params|
     "This is #{params.fetch("username")}!"
+  end
+
+  get '/submissions' do |params|
+    DB.exec_sql('select * from submissions')
+  end
+
+  get '/submissions/:name' do |params|
+    name = params.fetch('name')
+    user = DB.find_submission_by_name(name).fetch(0)
+    "the user is #{user.fetch('name')}"
   end
 end
